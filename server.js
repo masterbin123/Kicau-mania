8933d8f41f284b6af1fa50654959cbb6477946f7
masterbin123 <149627833+masterbin123@users.noreply.github.com>
2026-05-10 21:10:21 +0700

    Update server.js
    tq

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-{
-  "version": 2,
-  "builds": [
-    { "src": "server.js", "use": "@vercel/node" }
-  ],
-  "routes": [
-    { "src": "/(.*)", "dest": "server.js" }
-  ]
-}
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+
+app.use("/", createProxyMiddleware({
+  target: "https://167.71.199.11/",
+  changeOrigin: true,
+  secure: false
+}));
+app.listen(3000, () => {
+  console.log("Mirror berjalan di https://167.71.199.11/");
+});
